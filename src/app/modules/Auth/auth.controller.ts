7baa500a6afe416945f4c042e78d7fee4a29ed10
Ekramul <mdekramulhassan168@gmail.com>
2024-06-26 18:01:29 +0600

    Auth  admin  appError added

---
+++ src/app/modules/Auth/auth.controller.ts
@@ -0,0 +1,23 @@
+const loginUser = catchAsync(async (req, res) => {
+  const result = await AuthServices.loginUser(req.body);
+  const { refreshToken, accessToken, needsPasswordChange } = result;
+
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+  });
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User is logged in succesfully!',
+    data: {
+      accessToken,
+      needsPasswordChange,
+    },
+  });
+});
+
+export const AuthControllers = {
+  loginUser,
+};
--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,4 +1,7 @@
-export type TUser = {
+import { Model } from 'mongoose';
+import { roleEnum } from './user.constant';
+
+export interface TUser {
   name: string;
   email: string;
   password: string;
@@ -9,4 +12,20 @@ export type TUser = {
   role?: 'admin' | 'volunteer' | 'user';
   status?: 'in-progress' | 'blocked';
   isDeleted?: boolean;
-};
+}
+
+export interface UserModel extends Model<TUser> {
+  //instance methods for checking if the user exist
+  isUserExistsByEmail(email: string): Promise<TUser>;
+  //instance methods for checking if passwords are matched
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>;
+  isJWTIssuedBeforePasswordChanged(
+    passwordChangedTimestamp: Date,
+    jwtIssuedTimestamp: number,
+  ): boolean;
+}
+
+export type TUserRole = keyof typeof roleEnum;
